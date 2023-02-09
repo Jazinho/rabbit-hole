df3a194f01412ec17d205c64d8af1cebb5c6ada1
jpalucki <jan.palucki@miquido.com>
2023-02-09 15:40:01 +0100

    project template

---
+++ src/main/java/com/jpalucki/rabbithole/RabbitHoleApplication.java
@@ -0,0 +1,13 @@
+package com.jpalucki.rabbithole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitHoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitHoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpalucki/rabbithole/RabbitHoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpalucki.rabbithole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitHoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
